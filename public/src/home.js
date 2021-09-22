0168097342453e2c16049ffa0c0d1568443e79cd
Garrett Seeley <89324000+garrettseeley@users.noreply.github.com>
2021-09-22 10:11:21 -0500

    Added helper functions

--- public/src/home.js
+++ public/src/home.js
@@ -10,46 +10,58 @@ function getBooksBorrowedCount(books) {
   return books.filter((book) => !book.borrows[0].returned).length;
 }
 
+function sortAndSplice(array) {
+  return array
+    .sort((elementA, elementB) => elementB.count - elementA.count)
+    .splice(0, 5);
+}
+
 function getMostCommonGenres(books) {
   const array = [];
   const allGenres = books.map((book) => book.genre);
   const uniqueGenres = [...new Set(allGenres)];
   uniqueGenres.forEach((genre) => {
     let counter = 0;
-    allGenres.forEach((element) => {if (element === genre) {
-      counter++
-    }})
-    array.push({name: genre, count: counter})
-  })
-  return array.sort((elementA, elementB) => elementB.count - elementA.count).splice(0,5);
+    allGenres.forEach((element) => {
+      if (element === genre) {
+        counter++;
+      }
+    });
+    array.push({ name: genre, count: counter });
+  });
+  return sortAndSplice(array);
 }
 
 function getMostPopularBooks(books) {
   const array = [];
-  const borrowsElement = books.map((book) => book.borrows)
+  const borrowsElement = books.map((book) => book.borrows);
   books.forEach((book) => {
     let counter = 0;
-    let borrowed = book.borrows
-  borrowsElement.forEach((element) => {
-    counter = borrowed.length
-  })
-  array.push({name: book.title, count: counter})
-  })
-  return array.sort((elementA, elementB) => elementB.count - elementA.count).splice(0,5);
+    let borrowed = book.borrows;
+    borrowsElement.forEach((element) => {
+      counter = borrowed.length;
+    });
+    array.push({ name: book.title, count: counter });
+  });
+  return sortAndSplice(array);
 }
 
 function getMostPopularAuthors(books, authors) {
-  const array = []
+  const array = [];
   authors.forEach((auth) => {
     counter = 0;
     books.forEach((book) => {
-      let borrowed = book.borrows
+      let borrowed = book.borrows;
       if (book.authorId === auth.id) {
-      counter = borrowed.length
-    }})
-    array.push({name: `${auth.name.first} ${auth.name.last}`, count: counter})
-  })
-  return array.sort((elementA, elementB) => elementB.count - elementA.count).splice(0,5);
+        counter = borrowed.length;
+      }
+    });
+    array.push({
+      name: `${auth.name.first} ${auth.name.last}`,
+      count: counter,
+    });
+  });
+  return sortAndSplice(array);
 }
 
 module.exports = {
